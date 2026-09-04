3e909e75af29d2d035857e4f16ed878a95111175
Jin Yu <260294@sit.singaporetech.edu.sg>
2026-09-04 10:51:59 +0800

    Change to interactive profile creator

--- hello.py
+++ hello.py
@@ -1,24 +1,9 @@
-print("=======================")
-print("Welcome here")
-print("My first post!")
-print("=======================")
+username = input("Enter your username: ")
+age = input ("Enter your age: ")
+category =  input("Enter Content Category: ")
 
-
-username = "Cool_Yu"
-bio = "Fun Programmer"
-followers = 100
-
-
-followers += 50
-print("Day 1:", followers)
-
-followers += 20 
-print("Day 2:", followers)
-
-followers -= 10
-print("Day 3:", followers)
-
-
-print("Username: ",username)
-print("Bio: ", bio)
-print("Followers: ",followers)
+print("\nInstagram Profile")
+print("===================")
+print("Username: ", username)
+print("Age: ", age)
+print("Category: ", category)
