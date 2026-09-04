59cd4554e440aaa6a612ec91714cda1300ffb2f4
Jin Yu <260294@sit.singaporetech.edu.sg>
2026-09-04 10:42:02 +0800

    Second post

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ print("=======================")
 print("Welcome here")
 print("My first post!")
 print("=======================")
+
+
+username = "Cool_Yu"
+bio = "Fun Programmer"
+followers = 100
+
+print("Username: ",username)
+print("Bio: ", bio)
+print("Followers: ",followers)
